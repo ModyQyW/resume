66514f51982815471be55ad358a53bb3ed47205b
Rui Wu <wurui.dev@gmail.com>
2024-09-12 15:50:14 +0800

    修复头部样式没有正确应用 sticky 的问题

--- components/Header.vue
+++ components/Header.vue
@@ -6,7 +6,7 @@ defineOptions({
 
 <template>
   <header
-    class="sticky h-16 print:hidden border-b flex items-center justify-between gap-4"
+    class="sticky top-0 z-50 h-16 print:hidden border-b flex items-center justify-between gap-4"
   >
     <div class="flex-1">
       <Logo></Logo>
